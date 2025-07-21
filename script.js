c7ffe149a6d482d24cd1110f1ea398a147a7e60c
Imesh Munasinghe <imesh5lk@gmail.com>
2025-07-21 23:27:42 +0530

    Added functions for selecting answers

--- script.js
+++ script.js
@@ -81,7 +81,36 @@ function resetState() {}
         } else {
             selectedButton.classList.add("incorrect");
         }
-    
+        Array.from(answerButtons.children).forEach(button => {
+            if (button.dataset.correct === "true") {
+                button.classList.add("correct");
+            } 
+            button.disabled = true;
+        });
+        nextButton.style.display = "block";
     }
+
+    function showScore(){
+    resetState();
+    questionElemment.innerHTML= 'You scored ${score} out of ${questions.length}!';
+    nextButton.innerHTML="Play Again";
+    nextButton.style.display = "block";
+    }
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+    nextButton.addEventListener("click", () => {
+        if(currentQuestionIndex<questions.length){
+            handleNextButton();
+        }else{
+            startQuiz();
+        }
+    });
 
 startQuiz();
